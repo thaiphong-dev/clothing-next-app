ea5ebd3194d4a5f9caa8a4080fd1556b40198148
hh-Nghiep <nghiephh2000@gmail.com>
2022-01-10 15:33:14 +0700

    Nghiep fix header + footer

--- components/common/header/header.js
+++ components/common/header/header.js
@@ -22,31 +22,6 @@ export default function Header() {
         {/* <!-- Header desktop --> */}
         <div className="container-menu-desktop">
           {/* <!-- Topbar --> */}
-          <div className="top-bar">
-            <div className="content-topbar flex-sb-m h-full container">
-              <div className="left-top-bar">
-                Free shipping for standard order over $100
-              </div>
-
-              <div className="right-top-bar flex-w h-full">
-                <a href="#" className="flex-c-m trans-04 p-lr-25">
-                  Help & FAQs
-                </a>
-
-                <a href="#" className="flex-c-m trans-04 p-lr-25">
-                  My Account
-                </a>
-
-                <a href="#" className="flex-c-m trans-04 p-lr-25">
-                  EN
-                </a>
-
-                <a href="#" className="flex-c-m trans-04 p-lr-25">
-                  USD
-                </a>
-              </div>
-            </div>
-          </div>
 
           <div className={scroll}>
             <nav className="limiter-menu-desktop container">
--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,9 @@ import "../styles/Home.module.css";
 import Header from "./../components/common/header/header"
 export default function Home() {
   return (
-    <div>
-      {/* <Header /> */}
+    <div style={{height: "100vh"}}>
+      <Header />
+      <div style={{height: "1vh"}}></div> 
       <Footer />
     </div>
   );

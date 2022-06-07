37fcdc189cd6cf6de904179e509f5403f84c73ad
hh-Nghiep <71450931+hh-Nghiep@users.noreply.github.com>
2022-06-08 03:34:07 +0700

    Delete Contact, Features

--- components/common/header/header.js
+++ components/common/header/header.js
@@ -26,14 +26,12 @@ export default function Header() {
         {/* <!-- Header desktop --> */}
         <div className="container-menu-desktop">
           {/* <!-- Topbar --> */}
-
           <div className={scroll}>
             <nav className="limiter-menu-desktop container">
               {/* <!-- Logo desktop -->		 */}
               <a href="#" className="logo">
                 <Image src={logo1} alt="IMG-LOGO-DESKTOP" />
               </a>
-
               {/* <!-- Menu desktop --> */}
               <div className="menu-desktop">
                 <ul className="main-menu">
@@ -47,30 +45,18 @@ export default function Header() {
                       Shop
                     </Link>
                   </li>
-                  <li className="label1" data-label1="hot">
-                    <Link href="/features">Features</Link>
-                  </li>
-
                   <li>
                     <Link href="/blog" passHref>
                       Blog
                     </Link>
                   </li>
-
                   <li>
                     <Link href="/about" passHref>
                       About
                     </Link>
                   </li>
-
-                  <li>
-                    <Link href="/contact" passHref>
-                      Contact
-                    </Link>
-                  </li>
                 </ul>
               </div>
-
               {/* <!-- Icon header --> */}
               <div className="wrap-icon-header flex-w flex-r-m">
                 <Link href="/cart" passHref>
@@ -81,7 +67,6 @@ export default function Header() {
                     <i className="zmdi zmdi-shopping-cart"></i>
                   </div>
                 </Link>
-
                 <Link href="/login" passHref>
                   <div className="icon-header-item cl2 hov-cl1 trans-04 p-l-22 p-r-11 js-show-modal-search">
                     <i className="zmdi zmdi-account"></i>
@@ -91,7 +76,6 @@ export default function Header() {
             </nav>
           </div>
         </div>
-
         {/* <!-- Header Mobile --> */}
         <div className="wrap-header-mobile">
           {/* <!-- Logo moblie -->		 */}
@@ -100,13 +84,11 @@ export default function Header() {
               <Image alt="IMG-LOGO" src={logo1} />
             </a>
           </div>
-
           {/* <!-- Icon header --> */}
           <div className="wrap-icon-header flex-w flex-r-m m-r-15">
             <div className="icon-header-item cl2 hov-cl1 trans-04 p-r-11 js-show-modal-search">
               <i className="zmdi zmdi-search"></i>
             </div>
-
             <Link href="/cart" passHref>
               <div
                 className="icon-header-item cl2 hov-cl1 trans-04 p-r-11 p-l-10 icon-header-noti js-show-cart"
@@ -115,7 +97,6 @@ export default function Header() {
                 <i className="zmdi zmdi-shopping-cart"></i>
               </div>
             </Link>
-
             <a
               href="#"
               className="dis-block icon-header-item cl2 hov-cl1 trans-04 p-r-11 p-l-10 icon-header-noti"
@@ -124,7 +105,6 @@ export default function Header() {
               <i className="zmdi zmdi-favorite-outline"></i>
             </a>
           </div>
-
           {/* <!-- Button show menu --> */}
           <div className="btn-show-menu-mobile hamburger hamburger--squeeze">
             <span className="hamburger-box">
@@ -132,91 +112,29 @@ export default function Header() {
             </span>
           </div>
         </div>
-
         {/* <!-- Menu Mobile --> */}
         <div className="menu-mobile">
-          <ul className="topbar-mobile">
-            <li>
-              <div className="left-top-bar">
-                Free shipping for standard order over $100
-              </div>
-            </li>
-
-            <li>
-              <div className="right-top-bar flex-w h-full">
-                <a href="#" className="flex-c-m p-lr-10 trans-04">
-                  Help & FAQs
-                </a>
-
-                <a href="#" className="flex-c-m p-lr-10 trans-04">
-                  My Account
-                </a>
-
-                <a href="#" className="flex-c-m p-lr-10 trans-04">
-                  EN
-                </a>
-
-                <a href="#" className="flex-c-m p-lr-10 trans-04">
-                  USD
-                </a>
-              </div>
-            </li>
-          </ul>
-
           <ul className="main-menu-m">
             <li>
               <a href="index.html">Home</a>
-              <ul className="sub-menu-m">
-                <li>
-                  <a href="index.html">Homepage 1</a>
-                </li>
-                <li>
-                  <a href="home-02.html">Homepage 2</a>
-                </li>
-                <li>
-                  <a href="home-03.html">Homepage 3</a>
-                </li>
-              </ul>
               <span className="arrow-main-menu-m">
                 <i className="fa fa-angle-right" aria-hidden="true"></i>
               </span>
             </li>
-
             <li>
               <a href="product.html">Shop</a>
             </li>
-
-            <li>
-              <a
-                href="shoping-cart.html"
-                className="label1 rs1"
-                data-label1="hot"
-              >
-                Features
-              </a>
-            </li>
-
-            <li>
-              <a href="blog.html">Blog</a>
-            </li>
-
             <li>
               <a href="about.html">About</a>
             </li>
-
-            <li>
-              <a href="contact.html">Contact</a>
-            </li>
           </ul>
         </div>
-
         {/* <!-- Modal Search --> */}
         <div className="modal-search-header flex-c-m trans-04 js-hide-modal-search">
           <div className="container-search-header">
             <button className="flex-c-m btn-hide-modal-search trans-04 js-hide-modal-search">
               <Image src={iconClose2} alt="CLOSE" />
             </button>
-
             <form className="wrap-search-header flex-w p-l-15">
               <button className="flex-c-m trans-04">
                 <i className="zmdi zmdi-search"></i>
